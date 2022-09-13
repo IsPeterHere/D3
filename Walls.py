e630c47e9bc5953d26330c39af641ef7ec0c666f
PeterMai <89789068+IsPeterHere@users.noreply.github.com>
2022-09-13 16:37:20 +0100

    Update Walls.py

--- Walls.py
+++ Walls.py
@@ -6,21 +6,17 @@ import time
 class Tools:
 
     def player_load(self,x,y,z):
-        self.player_shell = d3.Cuboid_group([0,0,0])
+        self.player_shell = d3.Cuboid_group([x,y,z])
         self.Move_group = d3d.Dynamic(self.player_shell,self.solids)
 
-        self.body = d3.Cuboid([x,y,y],[10,10,10])
+        self.body = d3.Cuboid([x,y,z],[40,40,40])
         self.body.colour = "red"
-        self.leg  = d3.Cuboid([x,y,y-10],[5,5,10])
-        self.leg.colour = "red"
 
         self.player_shell.add(self.body)
-        self.player_shell.add(self.leg)
 
         self.Move_group.cuboid_group = self.player_shell
 
     def move(self,window):
-        self.Move_group.call(z = -2)
 
         if "A" in window.inputs:
             self.vr += -4
@@ -37,21 +33,14 @@ class Tools:
         if "q" in window.inputs:
             print(window.true_time)
                 
-        if "s" in window.inputs:
+        if "a" in window.inputs:
             self.Move_group.y_speed = 4
             self.direction = 120
         
-        elif "w" in window.inputs:
+        elif "d" in window.inputs:
             self.Move_group.y_speed = -4
             self.direction = 160
-            
-        if "a" in window.inputs:
-            self.Move_group.x_speed = 4
-            self.direction = 135
-            
-        elif "d" in window.inputs:
-            self.Move_group.x_speed = -4
-            self.direction = 230
+
 
         if self.jump == False:
             self.player_shell.move(z=-1)
@@ -83,7 +72,7 @@ class Tools:
         for x in range(self.mapsize):
             self.blocks.append([])
             for y in range(self.mapsize):
-                c = d3.Cuboid([x*40,y*40,200],[40,40,40])
+                c = d3.Cuboid([x*40,y*40,160],[40,40,40])
                 c.colour = "green"
                 self.solids.add(c)
                 window.add(c)
@@ -91,7 +80,7 @@ class Tools:
                 
         for x in range(self.mapsize):
             self.blocks.append([])
-            for y in range(self.mapsize-1):
+            for y in range(self.mapsize-2):
                 c = d3.Cuboid([x*40,0,40+y*40],[40,40,40])
                 c.colour = "red"
                 self.solids.add(c)
@@ -100,7 +89,7 @@ class Tools:
 
         for x in range(self.mapsize):
             self.blocks.append([])
-            for y in range(self.mapsize-1):
+            for y in range(self.mapsize-2):
                 c = d3.Cuboid([x*40,160,40+y*40],[40,40,40])
                 c.colour = "red"
                 self.solids.add(c)
@@ -111,8 +100,8 @@ class Game(Tools):
 
     def __init__(self):
 
-        self.hr = 160
-        self.vr = 210
+        self.hr = 90
+        self.vr = 185
 
         self.jump = False
         
@@ -127,16 +116,9 @@ class Game(Tools):
     def Set(self,window):
 
         window.clear()
-    
-        self.count = 0
-        
-        self.hr = 160
-        self.vr = 210
 
         self.solids = d3.Cuboid_group([0,0,0])
-        
-        self.player_load(0,0,0)
-
+        self.player_load(80,80,40)
         window.add(self.player_shell)
 
         self.tunnel(window)
@@ -148,6 +130,8 @@ class Game(Tools):
         window.vertical_rotation = self.vr
         window.centre(self.player_shell.centre)
         self.move(window)
+        self.Move_group.x_speed = 2
+
 
 game = Game()
 game.start()
